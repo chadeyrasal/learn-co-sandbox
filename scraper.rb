a8393d3b872150aee306266ff86286d8b1616495
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-12 18:08:03 +0000

    Automatically backed up by Learn

---
+++ scraper.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+doc.css(".site-header__hero__headline").text
+
+puts doc.css(".site-header__hero__headline")
